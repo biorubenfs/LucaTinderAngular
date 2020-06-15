a70cc7bcfd6f13edcca9312c6abbe9b07d3cb25a
biorubenfs <biorubenfs@hotmail.com>
2020-06-15 11:49:08 +0200

    Listar Contactos
    Añadida la funcionalidad para listar Perfiles. Se ha modificado el servicio, creado el componente, agregado un enlace y demás cosas. Se ha comprobado que funciona.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import{PerfilAddComponent} from './componentes/perfil-add/perfil-add.component';
+import { PerfilListaComponent } from './componentes/perfil-lista/perfil-lista.component';
 
 
 const routes: Routes = [
   {
     path:'add',
     component:PerfilAddComponent
+  },
+  {
+    path:'listar',
+    component:PerfilListaComponent
   }
 ];
 
