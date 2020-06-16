ca96f093f8c9a643a5511d166837755f1fa2e42a
alejandrojr98 <alejandrojr2011@gmail.com>
2020-06-16 12:31:35 +0200

    Arreglos necesarios
    Arreglos

--- src/app/componentes/inicio-perfil/inicio-perfil.component.ts
+++ src/app/componentes/inicio-perfil/inicio-perfil.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+import{Perfil} from '../../modelo/perfil';
+import{PerfilServicioService} from '../../servicio/perfil-servicio.service';
+import{LoginService} from '../../servicio/login.service';
+import{Router} from '@angular/router';
+
 @Component({
   selector: 'app-inicio-perfil',
   templateUrl: './inicio-perfil.component.html',
@@ -7,7 +12,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioPerfilComponent implements OnInit {
 
-  constructor() { }
+  perfil:Perfil=new Perfil();
+
+  constructor(private loginService: LoginService,
+    private router: Router,
+    private perfilServicio:PerfilServicioService) { }
 
   ngOnInit(): void {
   }
